8daf469022d76f1930bb701514561ef324cd7217
ikrishagarwal <68506145+ikrishagarwal@users.noreply.github.com>
2025-02-22 16:26:47 +0530

    fix: old code cleanup

--- api/404.ts
+++
@@ -1,6 +0,0 @@
-export function GET(request: Request) {
-  return Response.json({
-    status: 404,
-    message: "Page not found!",
-  });
-}
--- api/main.ts
+++
@@ -1,6 +0,0 @@
-export function GET(request: Request) {
-  return Response.json({
-    status: 200,
-    message: "Hello from Vercel!",
-  });
-}
