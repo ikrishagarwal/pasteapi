dfa55a428676bb73a464d7c70320b1e838782afb
ikrishagarwal <68506145+ikrishagarwal@users.noreply.github.com>
2025-02-26 01:15:12 +0530

    fix: fix logging

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,8 +1,12 @@
 // deno-lint-ignore no-explicit-any
 export function log(...args: any[]): void {
-  console.log(`${localeDateString()}`, `ERROR`, ...args);
+  // console.log(`${localeDateString()}`, `ERROR`, ...args);
+
+  // since deno deploy logs provide you with date and time
+  console.log(`ERROR`, ...args);
 }
 
+// deno-lint-ignore no-unused-vars
 function localeDateString(): string {
   const date = new Date();
   const dateString = date.toLocaleDateString();
